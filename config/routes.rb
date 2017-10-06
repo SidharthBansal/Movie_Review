24d56bab28b2bc4a78c382b1323b0f3dcc441646
Sidharth <bansal.sidharth2996@gmail.com>
2017-10-07 02:02:21 +0530

    Convert review ratings to start

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
   devise_for :users
   root to: "movies#index"
-  resources :movies
+  resources :movies do  
+  	resources :reviews,except: [:show, :index]
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
